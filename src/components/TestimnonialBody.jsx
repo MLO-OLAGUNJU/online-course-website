aa3d6ffdb691edbcc12242de0f4974fa41ed7e5e
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-04 01:37:04 +0100

    I updated the app and fixed a bug

--- src/components/TestimnonialBody.jsx
+++ src/components/TestimnonialBody.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import styled from "styled-components";
+import carousel1 from "../images/coursecard1.svg";
+import carousel2 from "../images/coursecard2.svg";
+import carousel3 from "../images/coursecard3.svg";
 
 const TestimnonialBody = () => {
-  return <div></div>;
+  const testimonialData = [
+    {
+      id: 1,
+      person: {
+        image: carousel1,
+        name: "Bill Wigss",
+        title: "Product Designer",
+      },
+      message:
+        "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World",
+    },
+  ];
+
+  return (
+    <>
+      <Wrapper>
+        {testimonialData.map((testimonial) => (
+          <Testimonial key={testimonial.id}>
+            <p>{testimonial.message}</p>
+            <Flex>
+              <img src={testimonial.person.image} alt="Person" />
+              <div>
+                <span></span>
+                <span></span>
+              </div>
+            </Flex>
+          </Testimonial>
+        ))}
+      </Wrapper>
+    </>
+  );
 };
 
+const Wrapper = styled.div`
+  padding-top: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Testimonial = styled.div`
+  width: 426px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 8px;
+`;
 export default TestimnonialBody;

4de2e1b322159d8f61ff8475bc7311d9deba73c9
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-04 02:44:22 +0100

    I updated the app and fixed a bug

--- src/components/TestimnonialBody.jsx
+++ src/components/TestimnonialBody.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import carousel1 from "../images/carouselImg1.svg";
 import carousel2 from "../images/carouselImg2.svg";
 import carousel3 from "../images/carouselImg3.svg";
@@ -17,52 +20,64 @@ const TestimnonialBody = () => {
         "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World",
     },
     {
-      id: 1,
+      id: 2,
       person: {
-        image: carousel1,
-        name: "Bill Wigss",
-        title: "Product Designer",
+        image: carousel2,
+        name: "Kingston Smith",
+        title: "Web Designer",
       },
       message:
         "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World",
     },
     {
-      id: 1,
+      id: 3,
       person: {
-        image: carousel1,
-        name: "Bill Wigss",
-        title: "Product Designer",
+        image: carousel3,
+        name: "Warner Jude",
+        title: "Web Developer",
       },
       message:
         "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World",
     },
     {
-      id: 1,
+      id: 4,
       person: {
-        image: carousel1,
-        name: "Bill Wigss",
-        title: "Product Designer",
+        image: carousel3,
+        name: "Warner Jude",
+        title: "Web Developer",
       },
       message:
         "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World",
     },
   ];
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true, // Enable autoplay
+    autoplaySpeed: 3000,
+  };
+
   return (
     <>
       <Wrapper>
-        {testimonialData.map((testimonial) => (
-          <Testimonial key={testimonial.id}>
-            <p>{testimonial.message}</p>
-            <Flex>
-              <img src={testimonial.person.image} alt="Person" />
-              <div>
-                <span>{testimonial.person.name}</span>
-                <span>{testimonial.person.title}</span>
-              </div>
-            </Flex>
-          </Testimonial>
-        ))}
+        <Slider {...settings}>
+          {testimonialData.map((testimonial) => (
+            <Testimonial key={testimonial.id}>
+              <p>{testimonial.message}</p>
+              <Flex>
+                <img src={testimonial.person.image} alt="Person" />
+                <div>
+                  <span>{testimonial.person.name}</span>
+                  <span>{testimonial.person.title}</span>
+                </div>
+              </Flex>
+            </Testimonial>
+          ))}
+        </Slider>
       </Wrapper>
     </>
   );
@@ -74,18 +89,30 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   padding-bottom: 40px;
+  gap: 30px;
+  .slick-slider {
+    overflow: hidden;
+  }
 `;
 const Testimonial = styled.div`
-  width: 426px;
-  padding: 20px;
+  padding: 50px 150px;
+  padding-top: 70px;
   background-color: #fff;
   border-radius: 8px;
+  @media (max-width: 768px) {
+    padding: 30px;
+  }
+  p {
+    @media (max-width: 768px) {
+      font-size: 13px;
+    }
+  }
 `;
 const Flex = styled.div`
   display: flex;
   margin-top: 10px;
   align-items: center;
-  justify-content: start;
+  justify-content: center;
   gap: 10px;
   div {
     display: flex;
@@ -104,4 +131,5 @@ const Flex = styled.div`
     }
   }
 `;
+
 export default TestimnonialBody;
--- src/components/TestimonialHeader.jsx
+++ src/components/TestimonialHeader.jsx
@@ -25,6 +25,9 @@ const Header = styled.div`
   p {
     width: 700px;
     margin-top: 40px;
+    @media (max-width: 768px) {
+      max-width: 330px;
+    }
   }
 `;
 export default TestimonialHeader;

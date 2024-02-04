4cd58440f6962d426b9cdfae1449e8e13ff8d721
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-04 01:48:31 +0100

    I updated the app and fixed a bug

--- src/components/TestimnonialBody.jsx
+++ src/components/TestimnonialBody.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import styled from "styled-components";
-import carousel1 from "../images/coursecard1.svg";
-import carousel2 from "../images/coursecard2.svg";
-import carousel3 from "../images/coursecard3.svg";
+import carousel1 from "../images/carouselImg1.svg";
+import carousel2 from "../images/carouselImg2.svg";
+import carousel3 from "../images/carouselImg3.svg";
 
 const TestimnonialBody = () => {
   const testimonialData = [
+    {
+      id: 1,
+      person: {
+        image: carousel1,
+        name: "Bill Wigss",
+        title: "Product Designer",
+      },
+      message:
+        "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World",
+    },
+    {
+      id: 1,
+      person: {
+        image: carousel1,
+        name: "Bill Wigss",
+        title: "Product Designer",
+      },
+      message:
+        "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World",
+    },
+    {
+      id: 1,
+      person: {
+        image: carousel1,
+        name: "Bill Wigss",
+        title: "Product Designer",
+      },
+      message:
+        "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World",
+    },
     {
       id: 1,
       person: {
@@ -27,8 +57,8 @@ const TestimnonialBody = () => {
             <Flex>
               <img src={testimonial.person.image} alt="Person" />
               <div>
-                <span></span>
-                <span></span>
+                <span>{testimonial.person.name}</span>
+                <span>{testimonial.person.title}</span>
               </div>
             </Flex>
           </Testimonial>
@@ -43,11 +73,35 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  padding-bottom: 40px;
 `;
 const Testimonial = styled.div`
   width: 426px;
   padding: 20px;
   background-color: #fff;
   border-radius: 8px;
+`;
+const Flex = styled.div`
+  display: flex;
+  margin-top: 10px;
+  align-items: center;
+  justify-content: start;
+  gap: 10px;
+  div {
+    display: flex;
+    align-items: start;
+    justify-content: start;
+    flex-direction: column;
+    span {
+      text-align: start;
+      &:nth-child(1) {
+        font-weight: 600;
+      }
+      &:nth-child(2) {
+        color: rgba(0, 0, 0, 0.5);
+      }
+      font-size: 12px;
+    }
+  }
 `;
 export default TestimnonialBody;
